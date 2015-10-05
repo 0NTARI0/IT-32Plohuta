9f6aeca5792f3423edc2e800b57e905001d10712
0NTARI0 <dimaploxyta@gmail.com>
2015-10-05 12:06:02 +0300

    Додана кнопка

--- IT32_Plohuta/IT32_Plohuta/Form1.cs
+++ IT32_Plohuta/IT32_Plohuta/Form1.cs
@@ -16,5 +16,10 @@ namespace IT32_Plohuta
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
